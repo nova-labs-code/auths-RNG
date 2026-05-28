de7eb44af66c3ab86262edbe04f0030993f63909
auth <skunkolee@gmail.com>
2026-05-27 19:49:08 -0600

    fixes and stuff yayy

---
+++ playwright.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  projects: [
+    {
+      name: 'chromium',
+      use: { browserName: 'chromium' },
+    },
+    {
+      name: 'firefox',
+      use: { browserName: 'firefox' },
+    },
+  ],
+});
